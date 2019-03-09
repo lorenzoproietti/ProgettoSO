3b5100c66de00f6f80eed135be68d2b6b3a7ca33
lorenzoproietti <lorenzoproietti16@outlook.it>
2019-03-09 14:36:29 +0100

    Added sem_wait

--- disastrOS_semwait.c
+++ disastrOS_semwait.c
@@ -5,7 +5,31 @@
 #include "disastrOS_syscalls.h"
 #include "disastrOS_semaphore.h"
 #include "disastrOS_semdescriptor.h"
+#include "disastrOS_globals.h"
 
 void internal_semWait(){
-  // do stuff :)
+
+  int fd = running->syscall_args[0];
+  SemDescriptor*  sem_d = SemDescriptorList_byFd(&running->sem_descriptors, fd);
+  if(!sem_d) {
+    running->syscall_retvalue = DSOS_ESEMWAIT_SEMD_NOT_IN_PROCESS;
+    return;
+  }
+
+  Semaphore* sem = sem_d->semaphore;
+  assert(sem);
+  sem->count--;
+  PCB* temp = running;
+  if(sem->count < 0) {
+    SemDescriptorPtr* sem_d_ptr = SemDescriptorPtr_alloc(sem_d);
+    assert(sem_d_ptr);
+    List_insert(&sem->waiting_descriptors, sem->waiting_descriptors.last, (ListItem*)sem_d_ptr);
+    running->status = Waiting;
+    List_insert(&waiting_list, waiting_list.last, (ListItem*)running);
+    running = (PCB*)List_detach(&ready_list,ready_list.first);
+  }
+
+  temp->syscall_retvalue = 0;
+  return;
+
 }
