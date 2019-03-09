3063fac3567a3361a00d7d7d4ad990b07517b329
lorenzoproietti <lorenzoproietti16@outlook.it>
2019-03-09 12:05:09 +0100

    Addition of semaphore function signatures

--- disastrOS.h
+++ disastrOS.h
@@ -38,3 +38,9 @@ int disastrOS_destroyResource(int resource_id);
 
 // debug function, prints the state of the internal system
 void disastrOS_printStatus();
+
+// syscall for semaphores
+int disastrOS_semOpen(int semnum);
+int disastrOS_semClose(int semnum);
+int disastrOS_semPost(int semnum);
+int disastrOS_semWait(int semnum);
