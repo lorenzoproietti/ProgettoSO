7feffa9e690eab6b5645b6492c9238754732353b
lorenzoproietti <lorenzoproietti16@outlook.it>
2019-03-09 12:08:25 +0100

    Added test for semopen and semclose

--- disastrOS_test.c
+++ disastrOS_test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <poll.h>
+#include <assert.h>
 
 #include "disastrOS.h"
 
@@ -15,17 +16,16 @@ void sleeperFunction(void* args){
 
 void childFunction(void* args){
   printf("Hello, I am the child function %d\n",disastrOS_getpid());
-  printf("I will iterate a bit, before terminating\n");
-  int type=0;
-  int mode=0;
-  int fd=disastrOS_openResource(disastrOS_getpid(),type,mode);
-  printf("fd=%d\n", fd);
-  printf("PID: %d, terminating\n", disastrOS_getpid());
-
   for (int i=0; i<(disastrOS_getpid()+1); ++i){
-    printf("PID: %d, iterate %d\n", disastrOS_getpid(), i);
-    disastrOS_sleep((20-disastrOS_getpid())*5);
+    int res = disastrOS_semOpen(i);
+    assert(res >= 0);
   }
+  disastrOS_printStatus();
+  for (int i=0; i<(disastrOS_getpid()+1); ++i){
+    int res = disastrOS_semClose(i);
+    assert(!res);
+  }
+  disastrOS_printStatus();
   disastrOS_exit(disastrOS_getpid()+1);
 }
 
